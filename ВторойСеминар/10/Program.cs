bf7677fefd4616bb64b2614b14820c199cbcd9a3
Nikita <mitichkin0303@mail.ru>
2023-09-23 19:22:33 +0200

    Доработка решения задачи 10

--- ВторойСеминар/10/Program.cs
+++ ВторойСеминар/10/Program.cs
@@ -4,7 +4,7 @@
 918 -> 1*/
 
 System.Console.WriteLine("Введите трехзначное число:");
-int num = int.Parse(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 
 int a = num / 10;
 int b = a% 10;
