5341c1df78c1fab3a37c90840c5a76ada04d8768
Nikita <mitichkin0303@mail.ru>
2023-09-23 18:59:01 +0200

    Подготовка дз 2

---
+++ ПервыйСеминар/8/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.*/
+
+
+Console.WriteLine("Введите число N:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Четные числа от 1 до " + n + ".");
+        
+        for (int i = 2; i <= n; i += 2) {
+            Console.WriteLine(i);
+        }
